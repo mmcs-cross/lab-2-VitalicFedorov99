9a0e2d0d70733f64a8f7670188b02fe901e97c97
VitalicFedorov99 <52524697+VitalicFedorov99@users.noreply.github.com>
2021-12-28 22:42:30 +0300

    лабораторная 2
    Тудушница готова

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -3,8 +3,8 @@ const GAUGE_MAX = 329
 function setGaugePercent($node, percent) {
   const $gaugeCircle = $node.querySelector('.gauge__cirlce-arc')
   const $gaugePercent = $node.querySelector('.gauge__percent')
-  
-  const value = GAUGE_MAX * (percent / 100) 
+
+  const value = GAUGE_MAX * (percent / 100)
 
   $gaugeCircle.setAttribute('stroke-dasharray', `${value} ${GAUGE_MAX}`)
   $gaugePercent.innerText = percent
@@ -16,6 +16,19 @@ function saveState(state) {
 
 function getStoredStateOrDefault(defaultState) {
   const stateAsStr = localStorage.getItem('todayAppState')
+  if (stateAsStr) {
+    return JSON.parse(stateAsStr)
+  } else {
+    return defaultState
+  }
+}
+
+function saveTasks(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getStoredTasksOrDefault(defaultState) {
+  const stateAsStr = localStorage.getItem('tasks')
   if (stateAsStr) {
     return JSON.parse(stateAsStr)
   } else {
--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,118 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const state = getStoredStateOrDefault({ 
-    counter: 40
-  })
+localStorage.clear()
 
-  const $incrButton = document.querySelector('.incr')
-  const $decrButton = document.querySelector('.decr')
+const noTasks = document.querySelector('.whatsUp')
+const todoTasks = document.querySelector('.tasks')
+const allDone = document.querySelector('.allDone')
+const gaugeCircle = document.querySelector('.gauge')
+const $gauge = document.querySelector('.gauge')
+const addItem = document.querySelector('.add__btn')
+const taskCounter = document.querySelector('.counter')
 
-  const $gauge = document.querySelector('.gauge')
-  setGaugePercent($gauge, state.counter)
-  
-  $incrButton.addEventListener('click', function() {
-    state.counter = Math.min(state.counter + 10, 100)
-    saveState(state)
-    setGaugePercent($gauge, state.counter)
-  })
+let tasks = JSON.parse(localStorage.getItem('tasks')) || []
 
-  $decrButton.addEventListener('click', function() {
-    state.counter = Math.max(state.counter - 10, 0)
-    saveState(state)
-    setGaugePercent($gauge, state.counter)
-  })
+const state = getStoredStateOrDefault({
+  counter: 40,
 })
+
+function TaskId() {
+  if (tasks.length) {
+    return tasks[tasks.length - 1].id + 1
+  }
+
+  return 1
+}
+
+function addTask__btn(event) {
+  event.preventDefault()
+  const value = addItem.value
+  addItem.value = ''
+  const newTask = {
+    id: TaskId(),
+    value,
+    checked: false,
+  }
+  tasks.push(newTask)
+  taskProcessing()
+  saveTasks(tasks)
+}
+
+function taskRemove(id) {
+  tasks = tasks.filter((elem) => elem.id != id)
+  taskProcessing()
+  saveTasks(tasks)
+}
+
+function taskProcessing() {
+  if (tasks.length) {
+    const allTasksDone = tasks.every((task) => task.checked)
+    if (allTasksDone) {
+      noTasks.style.display = 'none'
+      todoTasks.style.display = 'none'
+      gaugeCircle.style.display = 'none'
+      allDone.style.display = 'block'
+      taskCounter.style.display = 'none'
+    } else {
+      noTasks.style.display = 'none'
+      todoTasks.style.display = 'block'
+      gaugeCircle.style.display = 'block'
+      allDone.style.display = 'none'
+      taskCounter.style.display = 'flex'
+      templateTasks()
+      taskToDoCounter()
+    }
+  } else {
+    noTasks.style.display = 'block'
+    todoTasks.style.display = 'none'
+    gaugeCircle.style.display = 'none'
+    allDone.style.display = 'none'
+    taskCounter.style.display = 'none'
+  }
+}
+
+function templateTasks() {
+  if (tasks.length) {
+    todoTasks.innerHTML = ''
+    tasks.forEach((task, index) => {
+      let value = task.value
+      const newItem = document.createElement('div')
+      newItem.classList.add('task__item')
+      newItem.innerHTML = `
+        <input type="checkbox" ${
+          task.checked ? 'checked="checked"' : ''
+        } class="custom-checkbox" id="check${index}" name="check" value="yes" onchange="itemChecked(${
+        task.id
+      }, event.target.checked)">
+        <label for="check${index}">${value}</label>
+        <div class="remove__button" onclick="taskRemove(${task.id})"></div>
+        `
+      todoTasks.appendChild(newItem)
+    })
+  }
+  progressCounter()
+}
+
+function progressCounter() {
+  let percent
+  if (tasks.length) {
+    const completedTasksCount = tasks.filter((task) => task.checked).length
+    percent = Math.round((completedTasksCount / tasks.length) * 100)
+  } else {
+    percent = 0
+  }
+  saveState(state)
+  setGaugePercent($gauge, percent)
+}
+
+function itemChecked(id, checked) {
+  const index = tasks.findIndex((task) => task.id === id)
+  tasks[index].checked = checked
+  taskProcessing()
+  saveTasks(tasks)
+}
+
+function taskToDoCounter() {
+  const activeTasksCount = tasks.filter((task) => task.checked == false).length
+  taskCounter.innerHTML = ` ${activeTasksCount} tasks to do`
+}
+
+taskProcessing()
